cf58567a70d893bd0dc305b9801673b916a1a052
Victor-Cordeiro <victorcordeirodesousa@gmail.com>
2025-02-09 10:04:10 -0300

    Migrations com Flyway

---
+++ 05_FirstStepsInJavawithSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/resources/db/migration/V2__Populate_Table_Person2.sql
@@ -0,0 +1,13 @@
+
+
+
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'Pedrto Canario', 'Marttttcos', 'male', 'Siltva'),
+	(2, 'Colatina', 'Gabriel', 'male', 'Cordeiro'),
+	(3, 'Colatina', 'Emilly', 'female', 'Cordeiro'),
+	(4, 'Colatina', 'Carol', 'female', 'Cordeiro'),
+	(5, 'Colatina', 'Ana', 'female', 'Cordeiro'),
+	(6, 'Colatina', 'Vitoria', 'female', 'Cordeiro'),
+	(7, 'Colatina', 'Eduarda', 'female', 'Cordeiro');
+
+
